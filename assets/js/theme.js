aa9064735e52673f35083859b82f901299fb68eb
Ajay Sarjoo <ajay.sarjoo@outlook.com>
2019-12-04 00:17:00 -0500

    added navbar transition

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -28,3 +28,27 @@ $(".back-to-cv").click(function() {
         scrollTop: $("#cv").offset().top},
         2000);
 });
+     
+   // var scroll_start = 0;
+   // var startchange = $('#about-me');
+   // var offset = startchange.offset();
+   //  // if (startchange.length){
+   //     $(document).scroll(function() { 
+   //        scroll_start = $(this).scrollTop();
+   //        if(scroll_start > offset.top - 50) {
+   //            $("#navBar").addClass('navBar-def');
+   //         } else {
+   //            $('#navBar').css('background-color', 'transparent');
+   //         }
+   //     });
+    // }
+$(window).on("scroll", function() {
+    var scrollPos = $(window).scrollTop();
+    var $home = $("#welcome");
+    if (scrollPos> $home.height() - 90) {   
+        $('#navBar').removeClass('navBar-solid').addClass('navBar-def');
+    } 
+    if (scrollPos < $home.height() - 90){   
+        $('#navBar').removeClass('navBar-def').addClass('navBar-solid');      
+    }
+});

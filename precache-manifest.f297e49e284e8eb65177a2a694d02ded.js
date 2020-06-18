e5458f56f6255bec1c4ddd0adb9336a0c63ef0d9
Ajay Sarjoo <ajay.sarjoo@outlook.com>
2020-06-18 17:35:29 -0400

    Updates

---
+++ precache-manifest.f297e49e284e8eb65177a2a694d02ded.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44221c0eb0a25b343c786ca6ccffa23a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5e8226f33517f5a80a5",
+    "url": "/static/css/main.c9f7ac0a.chunk.css"
+  },
+  {
+    "revision": "568047072a7af6e59944",
+    "url": "/static/js/2.09162f7f.chunk.js"
+  },
+  {
+    "revision": "952b4911f6658a339d1a5dbba536f7d0",
+    "url": "/static/js/2.09162f7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5e8226f33517f5a80a5",
+    "url": "/static/js/main.514eb249.chunk.js"
+  },
+  {
+    "revision": "6609d493881c194bf0c7",
+    "url": "/static/js/runtime-main.030b6613.js"
+  }
+]);

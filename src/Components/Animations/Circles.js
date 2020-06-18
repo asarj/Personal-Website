09824938ae277856ca70dd32f73f2aca85333bd0
Ajay Sarjoo <ajay.sarjoo@outlook.com>
2020-06-18 17:03:30 -0400

    react.js overhaul of website

---
+++ src/Components/Animations/Circles.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Circles.css';
+
+function Circles() {
+	return (
+		<div className="circles" style={{'z-index': -100}}>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </div>
+	);
+}
+
+export default Circles;
---
+++ src/Components/Interests/Interests.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Container, Card, Row, Col} from 'react-bootstrap';
+import './Interests.css';
+
+function Interests(props) {
+	return (
+		<section id="interests" style={{padding: '10px', 'padding-top': '50px', 'padding-bottom': '50px', alignItems: 'center', alignContent: 'center'}}>
+            <h2 class="text-center"><strong>INTERESTS</strong></h2>
+            <br />
+            <Container>
+                <Row mt-5>
+                    {props.interests && props.interests.map((interest, index) => (
+                        <Col lg={4} mb={4} sm={20} grid-margin>
+                            <Card mx-auto className="description-card wow fadeInUp" data-wow-delay="0.5s">
+                                <Card.Header><i className={`${interest.faicon} faicon`}></i></Card.Header>
+                                <Card.Body className="align-item-center justify-content-center ">
+                                    
+                                    <br />
+                                    <Card.Title>{interest.title}</Card.Title>
+                                    <Card.Text>
+                                        <br />
+                                        {interest.description}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                            <br />
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        </section>
+	);
+}
+
+export default Interests;
